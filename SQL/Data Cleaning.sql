cfb3ff63a9641e5da5825001889c0e58a5080d18
marcrng <59784362+marcrng@users.noreply.github.com>
2021-08-24 22:26:50 -0700

    Formatting

--- SQL/Data Cleaning.sql
+++ SQL/Data Cleaning.sql
@@ -8,8 +8,8 @@ and `Zip Code` = 98006;
 # Remove restaurants that aren't in King County, but show up in dataset.
 # These data points result in inaccurate results for certain cities due to incomplete data.
 delete from inspection_data
-where city in ('Baring', 'Buckley', 'Everett', 'Fall city', 'Lynnwood', 'Mount Vernon', 'Mukilteo', 'Puyallup',
-              'SNOHOMISH', 'TACOMA');
+where city in ('Baring', 'Buckley', 'Everett', 'Fall city', 'Lynnwood', 'Mount Vernon',
+               'Mukilteo', 'Puyallup', 'SNOHOMISH', 'TACOMA');
 
 # Update all 'SEA TAC' entries to 'SEATAC' to normalize data
 update inspection_data
