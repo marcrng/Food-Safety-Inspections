e6a773c99a7a8309a1d631b8bf0c2e2ee9141a4b
marcrng <59784362+marcrng@users.noreply.github.com>
2021-08-24 23:41:55 -0700

    Create fail ratio per zip

--- SQL/Dashboard 1.sql
+++ SQL/Dashboard 1.sql
@@ -1,18 +1,34 @@
+# Create a fail ratio for each city found by total # of 'Unsatisfactory' inspections / All inspections
+create view fail_ratio_city as
 with unsatisfactory_count as
          (
              select City,
-                    count(distinct Name) as fails
+                    count(Name) as fails
              from inspection_data
              where `Inspection Result` = 'Unsatisfactory'
              group by City
          )
-
 select i.City,
-       fails / count(distinct Name) as fail_ratio
+       fails / count(*) as fail_ratio
 from inspection_data i
 join unsatisfactory_count u on i.City = U.City
 group by I.City
 order BY fail_ratio desc;
 
-select distincT City
-from inspection_data
+# Create a fail ratio for each zip found by total # of 'Unsatisfactory' inspections / All inspections
+# NOTE: Algona zip 98001 has a ratio of 0.3631, while city of Algona returns a 0.6323. Large discrepancy--
+create view fail_ratio_zip as
+with unsatisfactory_count as
+         (
+             select `Zip Code`,
+                    count(Name) as fails
+             from inspection_data
+             where `Inspection Result` = 'Unsatisfactory'
+             group by `Zip Code`
+         )
+select i.`Zip Code`,
+       fails / count(*) as fail_ratio
+from inspection_data i
+join unsatisfactory_count u on i.`Zip Code` = U.`Zip Code`
+group by I.`Zip Code`
+order BY fail_ratio desc;
