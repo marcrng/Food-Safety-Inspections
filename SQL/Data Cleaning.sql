073358508451ba6a90285696665abe1a6f077074
marcrng <59784362+marcrng@users.noreply.github.com>
2021-08-24 22:22:30 -0700

    Remove restaurants that aren't in King County

--- SQL/Data Cleaning.sql
+++ SQL/Data Cleaning.sql
@@ -1,6 +1,12 @@
 # Update unlabeled city for Walla Walla Farms Cafe @ T-Mobile Newport 2
-UPDATE inspection_data
+update inspection_data
 
 set city = 'BELLEVUE'
 where city = '(none)'
 and `Zip Code` = 98006;
+
+# Remove restaurants that aren't in King County, but show up in dataset.
+# These data points result in inaccurate results for certain cities due to incomplete data.
+delete from inspection_data
+where city in ('Baring', 'Buckley', 'Everett', 'Fall city', 'Lynnwood', 'Mount Vernon', 'Mukilteo', 'Puyallup',
+              'SNOHOMISH', 'TACOMA');
