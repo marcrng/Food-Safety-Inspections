d64496bbfd8b733193f4f359b0f39494e2d7cca7
marcrng <59784362+marcrng@users.noreply.github.com>
2021-08-24 22:25:49 -0700

    Update all city = 'SEA TAC' entries to city = 'SEATAC'

--- SQL/Data Cleaning.sql
+++ SQL/Data Cleaning.sql
@@ -10,3 +10,8 @@ and `Zip Code` = 98006;
 delete from inspection_data
 where city in ('Baring', 'Buckley', 'Everett', 'Fall city', 'Lynnwood', 'Mount Vernon', 'Mukilteo', 'Puyallup',
               'SNOHOMISH', 'TACOMA');
+
+# Update all 'SEA TAC' entries to 'SEATAC' to normalize data
+update inspection_data
+set City = 'SEATAC'
+where city = 'SEA TAC';
